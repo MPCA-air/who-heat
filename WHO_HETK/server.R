adf5143d947b74d078e7e5d420c1db2c00030310
zross <zev@zevross.com>
2015-10-01 11:01:04 -0400

    turned off several packages and seems to work

--- WHO_HETK/server.R
+++ WHO_HETK/server.R
@@ -2,13 +2,13 @@ library(shiny)
 library(shinyBS)  # devtools::install_github("ebailey78/shinyBS", ref = "shinyBS3")
 library(ggplot2)
 library(grid)
-library(plyr)
-library(countrycode)
+#library(plyr)
+#library(countrycode)
 library(RColorBrewer)
 library(dplyr)
 #library(RSQLite)
-library(digest)
-library(stringr) # this was in the userful.R file, not sure how important
+#library(digest)
+#library(stringr) # this was in the userful.R file, not sure how important
 library(shinythemes)
 
 
@@ -17,21 +17,12 @@ options(shiny.maxRequestSize = 0.100*1024^2)
 
 shinyServer(function(input, output, session){  
   source("utils/initial_settings.R", local=TRUE)
-  #source("global.R", local=TRUE)
-  #drv <- dbDriver("SQLite")
-  #.connection<<-dbConnect(drv, "data/HEMTK.db")
-  
-  #maindata<<-dbGetQuery(.connection, "SELECT * FROM maindata")
 
-
-  #source("helper.R", local=TRUE)
   source("utils/get_filtered.R", local=TRUE)
-  #source("utils/health_indicator_list.R", local=TRUE)
-  #source("utils/hetkdb.R", local=TRUE)
+
   source("utils/get_plots.R", local=TRUE)
   source("utils/get_data.R", local=TRUE)
-  #source("utils/inequal.R", local=TRUE)
-  #source("utils/comparisonCountries.R", local=TRUE)
+
 
   source("ui/data_management.R", local=TRUE)
   source("ui/explore_inequality.R", local=TRUE)
